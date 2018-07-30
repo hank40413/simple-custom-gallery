46b85eb98cd57018666a6b781cbb10efec5bf21a
Hank <hank40413@gmail.com>
2018-07-30 18:03:07 +0800

    修改create畫面，及增加選擇圖片並預覽功能

--- view/create.php
+++ view/create.php
@@ -1,4 +1,30 @@
 <body>
-	<h1>Create</h1>
+	<h1>Upload</h1>
 	<a href="index.php?menu=main">Main</a>
+	<form action="" enctype="multipart/form-data">
+	   <input type="file" id="uploadInput" accept="image/gif, image/jpeg, image/png" style="display: none;" />
+	   <img id="uploadPreview" src="icon/icons8-add-image-96.png" style="cursor: pointer; margin-top: 10px;" />
+	</form>
 </body>
+
+<script>
+$(document).ready(function() {
+	$("#uploadPreview").on("click", function() {
+		$("#uploadInput").click();
+	});
+});
+
+$("#uploadInput").change(function(){
+	readURL(this);
+});
+
+function readURL(input){
+if(input.files && input.files[0]){
+	var reader = new FileReader();
+		reader.onload = function (e) {
+			$("#uploadPreview").attr('src', e.target.result);
+		}
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+</script>
