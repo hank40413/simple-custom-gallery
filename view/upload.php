5889b3a2a94dac443993a773000dcb26930aedac
Hank <hank40413@gmail.com>
2018-08-01 16:33:31 +0800

    成功實作上傳圖片並且轉檔成jpg檔，但尚未實作更新資料庫內容

--- view/upload.php
+++ view/upload.php
@@ -1,15 +1,68 @@
 <head>
 	<title>Upload</title>
+	<style>
+		.container {
+			display: flex;
+			width: 100%; /* Full width */
+			height: 100%; /* Full height */
+		}
+		.loader {
+			display: block;
+			border: 16px solid #f3f3f3;
+			border-radius: 50%;
+			border-top: 16px solid #929292;
+			width: 120px;
+			height: 120px;
+			margin: auto;
+			flex: center;
+			-webkit-animation: spin 2s linear infinite; /* Safari */
+			animation: spin 2s linear infinite;
+		}
+
+		/* Safari */
+		@-webkit-keyframes spin {
+			0% { -webkit-transform: rotate(0deg); }
+			100% { -webkit-transform: rotate(360deg); }
+		}
+
+		@keyframes spin {
+			0% { transform: rotate(0deg); }
+			100% { transform: rotate(360deg); }
+		}
+	</style>
 </head>
 <body>
-	<i id="back" class="material-icons" >arrow_back</i><br>
+	<div class="container">
+		<div class="loader"></div>
+	</div>
 	
 <?php
-	$imgData = $_POST["uploadImgBlob"];
-	$imgBlob = base64_decode(substr($imgData, strpos($imgData, ",") + 1));
+	$uploadImgName = "img/" . (String)time() . ".jpg";
+	
+	// try {
+		// $file = fopen($uploadImgName, "w");
+		// fclose($file);
 
-    $imagick = new Imagick();
-    $imagick->readImageBlob($imgBlob);
-	$imagick->setImageFormat("jpg");
+		$imgData = $_POST["uploadImgBlob"];
+		$imgBlob = base64_decode(substr($imgData, strpos($imgData, ",") + 1));
+
+		$imagick = new Imagick();
+		$imagick->readImageBlob($imgBlob);
+		$imagick->setImageFormat("jpg");
+		
+		$imgWidth = $imagick->getImageWidth();
+		$imgHeight = $imagick->getImageHeight();
+
+		
+		// $imagick->writeImage(realpath($uploadImgName));
+	// }
+	// catch(Exception $e) {
+		// echo "<script>alert('發生錯誤，請重試。')</script>";
+		// unlink($uploadImgName);
+	// }
+	// finally {
+		// echo "<script>window.location.href = 'index.php'</script>";
+	// }
+
 ?>
 </body>
